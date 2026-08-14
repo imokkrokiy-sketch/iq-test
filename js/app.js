1026d3d1abf4dbddab521e3c064ecde446c09f83
imokkrokiy-sketch <imokkrokiy@gmail.com>
2026-08-14 05:01:36 +0000

    Remove remaining debug alert

--- js/app.js
+++ js/app.js
@@ -139,9 +139,7 @@ function pickStratifiedQuestions(bank, count) {
 }
 
 function startTest() {
-  alert("DEBUG: bank=" + QUESTION_BANK.length + " testlen=" + TEST_LENGTH);
   state.questions = pickStratifiedQuestions(QUESTION_BANK, TEST_LENGTH);
-  alert("DEBUG: picked=" + state.questions.length);
   state.index = 0;
   state.results = [];
 
